8eaaa9d377984ae3032bbc35ed235894b041673f
Bonny Bakshi <bonnybakshi@Bonnys-MacBook-Pro.local>
2016-09-21 21:36:06 -0400

    code

--- index.php
+++ index.php
@@ -9,7 +9,63 @@
     <?php require 'logic.php' ?>
 </head>
 <body>
+<div class='container'>
+	<h1 class="center"> <img src="https://openclipart.org/download/190821/Cles-de-serrure-lock-keys.svg" width="50"/> xkcd Password Generator</h1>
+	<h3 class="center">CSCI E-15 Dynamic Web Applications - Project 2<br></h3>
+	<h2 class="passwordHolder center"> <?php echo $password; ?></h2>
+
+<form  class="center" action="index.php" method="GET">
+	<div class="form-group">
+	<label for="word_num"> Number of Words
+	<input type="number" min="2" max="8" step="1" name="word_num" id="word_num" value="">
+	<i>(Min 2 &amp; Max 8)</i>
+	</label>
+	</div>	
 	
+	<div class="form-group">
+	<label class="form-check-label">
+  	<input class="form-check-input" type="checkbox"  name="number" id="number" 
+	<?php if($config['number'])  echo "checked"; ?>
+  	> Include a number
+	</label>
+	</div>
+
+	<div class="form-group">
+	<label class="form-check-label">
+  		<input class="form-check-input" type="checkbox" name="symbol" id="symbol" 
+  		<?php if($config['symbol'])  echo "checked"; ?>
+  		> Include a symbol
+	</label>
+	</div>
+
+	<div class="form-group"> 
+    <label class="radio-inline">
+      <input name="case" type="radio" value="upper" 
+      <?php if ($config['case']== "upper") echo "checked"; ?> >
+      All upper case
+    </label>
+  
+    <label class="radio-inline">
+      <input name="case" type="radio" value="lower" 
+      <?php if ($config['case'] == "lower") echo "checked";?> >
+      All lower case
+    </label>
+  </div>
+
+	<div class="form-group">
+	<input type='submit' class='btn btn-default button' value='Generate Password'>
+	</div>			
+</form>
+
+<div class="center">					
+<p class='xkcd'>
+<p><a href='http://xkcd.com/936/'>Inspired by xkcd password strength</a></p>
+
+<a href='http://xkcd.com/936/'>
+<img class='xkcd center' src='http://imgs.xkcd.com/comics/password_strength.png' alt='xkcd password strength'>
+</a>
+</p>
+</div>
 
 	
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
---
+++ logic.php
@@ -0,0 +1,55 @@
+<?php 
+/* get the list of words into an array */
+$wordList =  file ( "./file/wordlist.txt" );
+
+$password = ""; /*variable to hold password*/
+
+# array to store user input 
+$config['word_num']= 2; /* minimum 2 words */
+$config['number']= false;
+$config['symbol']= false;
+$config['case']= false;
+
+/* loop through $_GET and check for input and record the value  */
+foreach ($_GET as $name => $value){
+	if ($value){
+		$config[$name]= $value;
+	}
+}
+
+/* generate password*/
+for ($i = 1; $i <= (int)($config['word_num']); $i++) { 
+	$index = rand(0, count($wordList) - 1);
+	echo $index . " ";
+	$word = $wordList[$index];
+	echo $word;
+	# use hyphen after second word 
+	if ($i >= 2) {
+		# if the user selects 'All upper case", convert the password into uppercase 
+		if($config['case'] == 'upper'){
+			$password .= "-" . strtoupper(trim($word));
+		}else{ # else keep it lower case 
+			$password .= "-" . trim($word);
+		}
+	}else{
+		# if the user selects 'All upper case", convert the password into uppercase
+		if($config['case'] == 'upper'){
+			$password .= strtoupper(trim($word));
+		}else{ # else keep it lower case 
+			$password .= trim($word);;
+		}
+		
+	}
+}
+/* append a random symbol from the given symbol array if 'add a symbol' option is checked*/
+$symbol = array("!","@","#","$","%","&");
+if($config['symbol']){
+	$index = rand(0, count($symbol) - 1);
+	$password .= $symbol[$index];
+}
+
+/* append a random number between 0 and 9 if 'add a number' option is checked*/ 
+if($config['number']){
+	$number = rand(0, 9);
+	$password .= $number;
+}
